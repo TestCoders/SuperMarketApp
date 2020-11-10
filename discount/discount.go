d894d947d7cf11fa4ded5ac97a120e6d7b68920d
Marcel Blijleven <marcelblijleven@gmail.com>
2020-11-10 15:50:58 +0100

    feat(discount): add default discounts

--- discount/discount.go
+++ discount/discount.go
@@ -4,3 +4,13 @@ type Discount struct {
 	Description string `json:"description"`
 	Percentage  int    `json:"percentage"`
 }
+
+var BonusDiscount = &Discount{
+	Description: "Bonuskorting",
+	Percentage:  20,
+}
+
+var OutOfDateDiscount = &Discount{
+	Description: "Overdatumkorting",
+	Percentage:  20,
+}
