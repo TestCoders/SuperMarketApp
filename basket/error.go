f2959affbdcda73c04c0dfe476e5f6d788d27603
Marcel Blijleven <marcelblijleven@gmail.com>
2020-11-10 15:09:12 +0100

    chore(basket): add new error

--- basket/error.go
+++ basket/error.go
@@ -3,5 +3,6 @@ package basket
 import "errors"
 
 var (
-	ErrBasketNotFound = errors.New("basket not found")
+	ErrBasketNotFound           = errors.New("basket not found")
+	ErrRepositoryNotInitialised = errors.New("repository not initialised")
 )
